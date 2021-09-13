171a3f369bc8d1fc1efdb3ff411606ffef66183f
cemalsenel <senelcemal61@gmail.com>
2021-09-14 00:11:50 +0200

    recursive method : fibonacci, factorial

--- codingChallenge/src/Coding_1.java
+++ codingChallenge/src/Coding_1.java
@@ -1,7 +1,9 @@
 public class Coding_1 {
     public static void main(String[] args) {
 
-        recur(4);
+//        recur(4);
+//        System.out.println(fact(-4));
+        System.out.println(fibo(6));
     }
 
     public static void recur(int doll){
@@ -13,5 +15,26 @@ public class Coding_1 {
             recur(doll);
 
         }
+    }
+
+    public static int fact(int num){
+        if(num < 1){
+            return -1;
+        }
+        if ( num == 0 || num == 1){
+            return 1;
+        }
+        return num * fact(num-1);
+
+    }
+
+    public static int fibo(int num){
+        if(num < 0){
+            return -1;
+        }
+        if(num == 0 || num == 1){
+            return num;
+        }
+        return fibo(num-1) + fibo (num-2);
     }
 }
