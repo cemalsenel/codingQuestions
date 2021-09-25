05b91382f399a762143f6afce77edf3c2ab47ea7
cemalsenel <senelcemal61@gmail.com>
2021-09-25 09:30:13 +0200

    Array Questions: check arrays if they are permuation of each others

--- codingChallenge/src/ArrayQuestions.java
+++ codingChallenge/src/ArrayQuestions.java
@@ -22,8 +22,13 @@ public class ArrayQuestions {
 //        System.out.println(pairs);
 
         //5.Question: check array unique or not
-        int[] check = {1, 2, 3, 4};
-        System.out.println("Is array unique : " + isUnique(check));
+        int[] check = {1, 2, 3, 4, 4};
+//        System.out.println("Is array unique : " + isUnique(check));
+
+        //6.Question : Permutation
+        int[] array1 = {1, 2, 3, 4, 5};
+        int[] array2 = {5, 1, 2, 3, 6};
+        System.out.println("Are arrays permutation : " + permutation(array1, array2));
     }
 
     public static void missingNumber(int[] intArray){
@@ -83,5 +88,28 @@ public class ArrayQuestions {
             }
         }
         return true;
+    }
+
+    public static boolean permutation(int[] array1, int[] array2){
+        if(array1.length != array2.length){
+            return false;
+        }
+        int sum1= 0;
+        int sum2= 0;
+        int mul1= 1;
+        int mul2= 1;
+
+        for(int i = 0; i<array1.length; i++){
+            sum1 += array1[i];
+            sum2 += array2[i];
+            mul1 *= array1[i];
+            mul2 *= array1[i];
+        }
+        if(sum1 == sum2 && mul1 == mul2){
+            return true;
+        }
+
+        return false;
+
     }
 }
