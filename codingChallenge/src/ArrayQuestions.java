c7e4cce7ae7b8304188649084a54b29c4a2b9c16
cemalsenel <senelcemal61@gmail.com>
2021-09-25 08:54:36 +0200

    Array Questions: Pairs/ Two Sum

--- codingChallenge/src/ArrayQuestions.java
+++ codingChallenge/src/ArrayQuestions.java
@@ -1,9 +1,16 @@
+import java.util.Arrays;
+
 public class ArrayQuestions {
     public static void main(String[] args) {
         //1.Question:Find the missing number in the array
         int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20};
-        missingNumber(intArray);
+//        missingNumber(intArray);
 
+        //2.Question: Pairs/Two Sum
+        int[] nums = {12, 17, 8, 1};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
 
 
     }
@@ -16,5 +23,18 @@ public class ArrayQuestions {
         }
         int difference = sum2 - sum1;
         System.out.println("Missing number is : " + difference);
+    }
+
+    public static int[] twoSum(int[] nums, int target){
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j< nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[] {i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution found!");
+
     }
 }
