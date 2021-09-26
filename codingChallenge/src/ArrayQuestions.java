ed3c7588736deb6f9adbb76870e4c93b2b065d42
cemalsenel <senelcemal61@gmail.com>
2021-09-26 21:51:43 +0200

    Array Questions: rotate matrix by 90 degrees - 2

--- codingChallenge/src/ArrayQuestions.java
+++ codingChallenge/src/ArrayQuestions.java
@@ -32,7 +32,9 @@ public class ArrayQuestions {
 
         //7.Question: Rotate matrix 90 degrees
         int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
-        rotateMatrix(matrix);
+//        rotateMatrix(matrix);
+//        System.out.println(Arrays.deepToString(matrix));
+        rotateImage(matrix);
         System.out.println(Arrays.deepToString(matrix));
     }
 
@@ -138,4 +140,28 @@ public class ArrayQuestions {
         }
         return true;
     }
+
+    public static int[][] rotateImage(int[][] matrix) {
+        int n = matrix.length;
+
+
+        for(int i = 0; i<n; i++){
+           for (int j = i; j<n; j++){
+               int temp = matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp;
+           }
+        }
+
+        for(int i = 0; i <n; i++){
+            for(int j = 0; j <(n/2); j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+        return matrix;
+    }
+    
+
 }
